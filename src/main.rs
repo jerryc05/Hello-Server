57520437ba32c0fcbdeff48c3fada25795ed2c3a
Ziyan Jerry Chen <jerryc443@gmail.com>
2019-11-23 11:41:08 -0500

    simplest running demo

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::any::Any;
 use std::env;
 
 use tokio;
@@ -28,8 +29,8 @@ async fn main() {
 
   loop {
     // Asynchronously wait for an inbound socket. f:off
-    let (mut tcp_stream, _) = listener.accept().await
-      .expect("Failed to accept new incoming TCP stream!"); // f:on
+    let (mut tcp_stream, socket_address) = listener.accept().await
+        .expect("Failed to accept new incoming TCP stream!"); // f:on
 
     /* And this is where much of the magic of this server happens. We
      * crucially want all clients to make progress concurrently, rather than
@@ -40,117 +41,26 @@ async fn main() {
      * which will allow all of our clients to be processed concurrently.
      */
     tokio::spawn(async move {
-      let (mut read_stream, mut write_stream) = tcp_stream.split();
-      let mut bytes_buffer = [0; 128];
-      let mut responded_http_version = false;
-      let mut http_version_write_all = None;
-//      let mut content_length = 0;
+      println!("\n\n\n--- Incoming TCP connection from {:?} ---", socket_address);
 
-      println!("[Incoming connection!] --- --- ---");
+      let (mut read_stream, mut write_stream) = tcp_stream.split();
+      let mut bytes_buffer = [0; 512];
 
       // In a loop, read data from the socket and write the data back.
       loop {
         let n = read_stream.read(&mut bytes_buffer)
                            .await.expect("Failed to read lines from socket!");
         if n > 0 {
-          if !responded_http_version {
-            responded_http_version = true;
-            http_version_write_all = Some(write_stream.write_all("HTTP/1.1".as_bytes()));
-          }
           print!("{}", unsafe { std::str::from_utf8_unchecked(&bytes_buffer) });
 
           if n < bytes_buffer.len() {
-            http_version_write_all.expect("Fail to get initialized instance of http_version_write_all!")
-              .await.expect("Failed to write HTTP Version!");
-//          tcp_stream.write_all(" 200 OK\r\n\r\nHello!".as_bytes())
-//                    .await.expect("Failed to write to TCP Stream!");
-            println!("write finished!");
+          tcp_stream.write_all("HTTP/1.1 200 OK\r\n\r\nHello!".as_bytes())
+                    .await.expect("Failed to write to TCP Stream!");
+            println!("write finished!",);
             break;
           }
         }
-//        if n <= bytes_buffer.len() { break; }
-
-//        let line_buffer = complete_request.lines().last()
-//                                          .expect("Failed to get last line of complete request!");
-//
-//        if line_buffer.to_ascii_uppercase().contains("CONTENT-LENGTH") {
-//          content_length = line_buffer[16..]
-//            .parse().expect("Failed to parse Content-Length into usize while streaming!");
-//          //
-//          // Begin parse body
-//        } else if line_buffer.is_empty() {
-//          let mut content_buffer = Vec::with_capacity(content_length);
-//
-//          buffered_reader.read_exact(&mut content_buffer).await
-//                         .expect("Failed to read content from socket!");
-//          println!("---\ncontent_buffer [{}]", unsafe {
-//            String::from_utf8_unchecked(content_buffer)
-//          });
-////          complete_request.push_str(
-////            std::str::from_utf8(&content_buffer)
-////              .expect("Failed to parse content to utf8!")
-////          );
-//          break;
-//        }
       }
-
-//      let http_request = HTTPRequest::from(complete_request.as_str());
-//      println!("{:?}\n---", http_request)
-
-//      let request =
-//        socket.write_all(&buffer[0..n])
-//              .await
-//              .expe
-
-
-//      let (read_end, mut write_end) = tcp_stream.split();
-//      let mut buffered_reader = BufReader::new(read_end);
-//      let mut complete_request = String::new();
-//      let mut content_length = 0;
-//
-//      // In a loop, read data from the socket and write the data back.
-//
-//
-//      // todo read until it gets "\r\n\r\n" and then parse content length, then read_exact until end.
-//
-//
-//      loop {
-//        let n = buffered_reader
-//          .read_line(&mut complete_request).await
-//          .expect("Failed to read lines from socket!");
-//        if n <= 0 { break; }
-//
-//        let line_buffer = complete_request.lines().last()
-//                                          .expect("Failed to get last line of complete request!");
-//
-//        if line_buffer.to_ascii_uppercase().contains("CONTENT-LENGTH") {
-//          content_length = line_buffer[16..]
-//            .parse().expect("Failed to parse Content-Length into usize while streaming!");
-//          //
-//          // Begin parse body
-//        } else if line_buffer.is_empty() {
-//          let mut content_buffer = Vec::with_capacity(content_length);
-//
-//          buffered_reader.read_exact(&mut content_buffer).await
-//                         .expect("Failed to read content from socket!");
-//          println!("---\ncontent_buffer [{}]", unsafe {
-//            String::from_utf8_unchecked(content_buffer)
-//          });
-////          complete_request.push_str(
-////            std::str::from_utf8(&content_buffer)
-////              .expect("Failed to parse content to utf8!")
-////          );
-//          break;
-//        }
-//      }
-//
-//      let http_request = HTTPRequest::from(complete_request.as_str());
-//      println!("{:?}\n---", http_request)
-//
-////      let request =
-////        socket.write_all(&buffer[0..n])
-////              .await
-////              .expect("Failed to write data to socket");
     });
   }
 }
