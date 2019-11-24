f7dda9b53f5970635a0f46207f6c7e33fcd2f7dd
Ziyan Jerry Chen <jerryc443@gmail.com>
2019-11-24 18:06:55 -0500

    add console debugging indicator

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 use async_std::{println, task};
 use async_std::net::{TcpListener, TcpStream};
+use chrono::{FixedOffset, Utc};
 use futures::StreamExt;
 
 mod http;
@@ -7,13 +8,20 @@ mod http;
 pub struct HelloServer {
   ip_addr: &'static str,
   port: u16,
+  local_timezone_offset: FixedOffset,
   callback: fn(TcpStream),
 }
 
 impl HelloServer {
-  pub fn new(ip_addr: &'static str, port: u16,
+  pub fn new(ip_addr: &'static str, port: u16, timezone_in_hr: i32,
              callback: fn(TcpStream)) -> HelloServer {
-    HelloServer { ip_addr, port, callback }
+    HelloServer {
+      ip_addr,
+      port,
+      local_timezone_offset: FixedOffset::east_opt(timezone_in_hr * 3600)
+        .expect(&format!("Failed to parse timezone UTC{}!", timezone_in_hr)),
+      callback,
+    }
   }
 
   pub fn run(self) {
@@ -27,13 +35,21 @@ impl HelloServer {
 
       loop {
         while let Some(stream) = incoming_tcp.next().await {
-          let callback = self.callback;
           let stream: TcpStream = stream
             .expect("Failed to get stream from incoming TCP connection!");
-          println!("\n\n---\nAccepting from [{:?}]!", stream).await;
 
+          println!("\u{250C}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}").await;
+          println!("\u{2502} Incoming TCP @ {:?} ",
+                   Utc::now().with_timezone(&self.local_timezone_offset)).await;
+          println!("\u{2502} Accepting from [{}] to [{}]",
+                   stream.peer_addr().expect("Failed to get peer address of stream!"),
+                   stream.local_addr().expect("Failed to get local address of stream!")).await;
+          println!("\u{251C}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}").await;
+
+          let callback = self.callback;
           task::spawn(async move {
             callback(stream);
+            std::println!("\u{2514}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}");
           });
         }
       }
--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,13 @@ const DEFAULT_PORT: u16 = 6006;
 const DEFAULT_IP_ADDRESS: &str = "127.0.0.1";
 
 fn main() {
+  example_server()
+}
+
+fn example_server() {
   HelloServer::new(
-    DEFAULT_IP_ADDRESS, DEFAULT_PORT,
+    DEFAULT_IP_ADDRESS, DEFAULT_PORT,-5,
     move |tcp_stream| {
-      println!("Accepted [{:?}] from closure!", tcp_stream);
+      println!("Accepted [stream] from closure!");
     }).run();
 }
