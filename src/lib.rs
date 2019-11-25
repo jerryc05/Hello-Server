07644e4dcf5903c2d3c1905066bd3dd0e6df2bfa
Ziyan Jerry Chen <jerryc443@gmail.com>
2019-11-24 19:13:37 -0500

    demo runs correctly

--- src/lib.rs
+++ src/lib.rs
@@ -9,12 +9,12 @@ pub struct HelloServer {
   ip_addr: &'static str,
   port: u16,
   local_timezone_offset: FixedOffset,
-  callback: fn(TcpStream),
+  callback: fn(TcpStream, &u128),
 }
 
 impl HelloServer {
   pub fn new(ip_addr: &'static str, port: u16, timezone_in_hr: i32,
-             callback: fn(TcpStream)) -> HelloServer {
+             callback: fn(TcpStream, &u128)) -> HelloServer {
     HelloServer {
       ip_addr,
       port,
@@ -32,24 +32,33 @@ impl HelloServer {
       println!("Listening on  [{}:{}]!", self.ip_addr, self.port).await;
 
       let mut incoming_tcp = listener.incoming();
+      let mut tcp_counter: u128 = 0;
 
       loop {
         while let Some(stream) = incoming_tcp.next().await {
           let stream: TcpStream = stream
             .expect("Failed to get stream from incoming TCP connection!");
 
-          println!("\u{250C}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}").await;
-          println!("\u{2502} Incoming TCP @ {:?} ",
+          const DASH: &str = "\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}";
+
+          println!("\u{250C}{}", DASH).await;
+          tcp_counter = (tcp_counter + 1) % (std::u128::MAX - 1);
+          let tcp_num: u128 = tcp_counter;
+          println!("\u{2502} Incoming TCP #{} @ {:?} ", tcp_num,
                    Utc::now().with_timezone(&self.local_timezone_offset)).await;
+
+          let peer_addr = stream.peer_addr().expect("Failed to get peer address of stream!");
           println!("\u{2502} Accepting from [{}] to [{}]",
-                   stream.peer_addr().expect("Failed to get peer address of stream!"),
+                   peer_addr,
                    stream.local_addr().expect("Failed to get local address of stream!")).await;
-          println!("\u{251C}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}").await;
+          println!("\u{2514}{}", DASH).await;
 
           let callback = self.callback;
           task::spawn(async move {
-            callback(stream);
-            std::println!("\u{2514}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}");
+            callback(stream, &tcp_num);
+            std::println!("\u{250C}{}", DASH);
+            std::println!("\u{2502} End of TCP connection #{} from [{}]", tcp_num, peer_addr);
+            std::println!("\u{2514}{}", DASH);
           });
         }
       }
--- src/main.rs
+++ src/main.rs
@@ -12,8 +12,17 @@ fn main() {
 
 fn example_server() {
   HelloServer::new(
-    DEFAULT_IP_ADDRESS, DEFAULT_PORT,-5,
-    move |tcp_stream| {
-      println!("Accepted [stream] from closure!");
+    DEFAULT_IP_ADDRESS, DEFAULT_PORT, -5,
+    move |tcp_stream, tcp_num| {
+      println!("Simulate processing 1/5 tcp stream #{}!", tcp_num);
+      std::thread::sleep(std::time::Duration::from_secs(1));
+      println!("Simulate processing 2/5 tcp stream #{}!", tcp_num);
+      std::thread::sleep(std::time::Duration::from_secs(1));
+      println!("Simulate processing 3/5 tcp stream #{}!", tcp_num);
+      std::thread::sleep(std::time::Duration::from_secs(1));
+      println!("Simulate processing 4/5 tcp stream #{}!", tcp_num);
+      std::thread::sleep(std::time::Duration::from_secs(1));
+      println!("Simulate processing 5/5 tcp stream #{}!", tcp_num);
+      std::thread::sleep(std::time::Duration::from_secs(1));
     }).run();
 }
