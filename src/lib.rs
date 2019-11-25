cfeb263590eb006733fa56a6e1e9f53f7fe5bc3c
Ziyan Jerry Chen <jerryc443@gmail.com>
2019-11-24 21:44:53 -0500

    stuck at `cannot be shared between threads safely`

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,24 @@
 use async_std::{println, task};
+use async_std::future::Future;
 use async_std::net::{TcpListener, TcpStream};
+use async_std::prelude::*;
 use chrono::{FixedOffset, Utc};
-use futures::StreamExt;
 
 mod http;
 
-pub struct HelloServer {
+pub struct HelloServer<T: Future + 'static + Send> {
   ip_addr: &'static str,
   port: u16,
   local_timezone_offset: FixedOffset,
-  callback: fn(TcpStream, &u128),
+  callback: fn(TcpStream, u128) -> T,
 }
 
-impl HelloServer {
-  pub fn new(ip_addr: &'static str, port: u16, timezone_in_hr: i32,
-             callback: fn(TcpStream, &u128)) -> HelloServer {
+impl<T: Future + Send + 'static> HelloServer<T> {
+  pub fn new(ip_addr: &'static str,
+             port: u16,
+             timezone_in_hr: i32,
+             callback: fn(TcpStream, u128) -> T,
+  ) -> HelloServer<T> {
     HelloServer {
       ip_addr,
       port,
@@ -40,25 +44,23 @@ impl HelloServer {
             .expect("Failed to get stream from incoming TCP connection!");
 
           const DASH: &str = "\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}";
+          tcp_counter = (tcp_counter + 1) % (std::u128::MAX - 1);
 
           println!("\u{250C}{}", DASH).await;
-          tcp_counter = (tcp_counter + 1) % (std::u128::MAX - 1);
-          let tcp_num: u128 = tcp_counter;
-          println!("\u{2502} Incoming TCP #{} @ {:?} ", tcp_num,
+          println!("\u{2502} Incoming TCP #{} @ {:?} ", tcp_counter,
                    Utc::now().with_timezone(&self.local_timezone_offset)).await;
 
           let peer_addr = stream.peer_addr().expect("Failed to get peer address of stream!");
-          println!("\u{2502} Accepting from [{}] to [{}]",
-                   peer_addr,
-                   stream.local_addr().expect("Failed to get local address of stream!")).await;
+          let local_addr = stream.local_addr().expect("Failed to get local address of stream!");
+          println!("\u{2502} Accepting from [{}] to [{}]", peer_addr, local_addr).await;
           println!("\u{2514}{}", DASH).await;
 
-          let callback = self.callback;
+//          let callback_func = self.callback;
           task::spawn(async move {
-            callback(stream, &tcp_num);
-            std::println!("\u{250C}{}", DASH);
-            std::println!("\u{2502} End of TCP connection #{} from [{}]", tcp_num, peer_addr);
-            std::println!("\u{2514}{}", DASH);
+//            callback_func(stream, tcp_counter).await;
+            println!("\u{250C}{}", DASH).await;
+            println!("\u{2502} End of TCP connection #{} from [{}]", tcp_counter, peer_addr).await;
+            println!("\u{2514}{}", DASH).await;
           });
         }
       }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#![feature(async_closure)]
+
 use hello_server::HelloServer;
 
 /// Default internal port
@@ -7,22 +9,56 @@ const DEFAULT_PORT: u16 = 6006;
 const DEFAULT_IP_ADDRESS: &str = "127.0.0.1";
 
 fn main() {
-  example_server()
+  _establish_connection_example();
+//  _echo_server_example();
 }
 
-fn example_server() {
+fn _establish_connection_example() {
+  use async_std::task::sleep;
+  use std::time::Duration;
+  use async_std::println;
+
   HelloServer::new(
     DEFAULT_IP_ADDRESS, DEFAULT_PORT, -5,
-    move |tcp_stream, tcp_num| {
-      println!("Simulate processing 1/5 tcp stream #{}!", tcp_num);
-      std::thread::sleep(std::time::Duration::from_secs(1));
-      println!("Simulate processing 2/5 tcp stream #{}!", tcp_num);
-      std::thread::sleep(std::time::Duration::from_secs(1));
-      println!("Simulate processing 3/5 tcp stream #{}!", tcp_num);
-      std::thread::sleep(std::time::Duration::from_secs(1));
-      println!("Simulate processing 4/5 tcp stream #{}!", tcp_num);
-      std::thread::sleep(std::time::Duration::from_secs(1));
-      println!("Simulate processing 5/5 tcp stream #{}!", tcp_num);
-      std::thread::sleep(std::time::Duration::from_secs(1));
+    async move |_tcp_stream, _tcp_num| {
+      println!("#{}: Simulate processing 1/5 tcp stream!", _tcp_num).await;
+      sleep(Duration::from_secs(1)).await;
+      println!("#{}: Simulate processing 2/5 tcp stream!", _tcp_num).await;
+      sleep(Duration::from_secs(1)).await;
+      println!("#{}: Simulate processing 3/5 tcp stream!", _tcp_num).await;
+      sleep(Duration::from_secs(1)).await;
+      println!("#{}: Simulate processing 4/5 tcp stream!", _tcp_num).await;
+      sleep(Duration::from_secs(1)).await;
+      println!("#{}: Simulate processing 5/5 tcp stream!", _tcp_num).await;
+      sleep(Duration::from_secs(1)).await;
+    }).run();
+}
+
+fn _echo_server_example() {
+  use async_std::{
+    io::*,
+    prelude::*,
+  };
+
+  HelloServer::new(
+    DEFAULT_IP_ADDRESS, DEFAULT_PORT, -5,
+    async move |_tcp_stream, _tcp_num| {
+      let mut lines = BufReader::new(_tcp_stream).lines();
+      while let Some(line) = lines.next().await {
+        println!("{}", line.expect("Failed to get line from lines.next()!"))
+//        let line = line?;
+//        let (dest, msg) = match line.find(':') {
+//          None => continue,
+//          Some(idx) => (&line[..idx], line[idx + 1..].trim()),
+//        };
+//        let dest: Vec<String> = dest.split(',').map(|name| name.trim().to_string()).collect();
+//        let msg: String = msg.to_string();
+//
+//        broker.send(Event::Message { // 4
+//          from: name.clone(),
+//          to: dest,
+//          msg,
+//        }).await.unwrap();
+      }
     }).run();
 }
